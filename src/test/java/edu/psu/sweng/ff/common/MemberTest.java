f4bc6c7af55e2a26c23c487f83542499c740545f
Casey Leonard <casey@amphibian.com>
2012-09-23 13:11:04 -0400

    better test

--- src/test/java/edu/psu/sweng/ff/common/MemberTest.java
+++ src/test/java/edu/psu/sweng/ff/common/MemberTest.java
@@ -2,6 +2,9 @@ package edu.psu.sweng.ff.common;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 
 /**
@@ -17,13 +20,30 @@ public class MemberTest
     	m.setLastName("lastname");
     	m.setPassword("password");
     	
-    	System.out.println(m.getPasswordHash());
+    	//System.out.println(m.getPasswordHash());
     	
     	assertEquals("firstname", m.getFirstName());
     	assertEquals("lastname", m.getLastName());
         
     }
     
+    @Test
+    public void testHash() {
+    	
+    	try {
+    		
+			String hash1 = Member.getHash("password");
+			String hash2 = Member.getHash("password");
+			
+			assertEquals(hash1, hash2);
+			
+		} catch (NoSuchAlgorithmException e) {
+			fail(e.getMessage());
+		} catch (UnsupportedEncodingException e) {
+			fail(e.getMessage());
+		}
+    	
+    }
     
     
 }
